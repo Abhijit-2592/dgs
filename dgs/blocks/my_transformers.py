efeec7daca4418e17ce28bdf34fd27f7aacf0a70
Abhijit Balaji <balaabhijit5@gmail.com>
2021-03-29 21:49:18 -0500

    Coattention module added
    a few bug fixes  in the GeneralTransformerEncoderLayer

--- dgs/blocks/my_transformers.py
+++ dgs/blocks/my_transformers.py
@@ -1,6 +1,7 @@
-from typing import Optional
-
 import torch
+import copy
+
+from typing import Optional
 from torch import nn
 from torch.nn import functional as F
 
@@ -15,7 +16,7 @@ class GeneralTransformerEncoderLayer(nn.Module):
         dim_feedforward: the dimension of the feedforward network model (default=2048).
         dropout: the dropout value (default=0.1).
         activation: the activation function of intermediate layer, relu or gelu (default=relu).
-        layer_norm_eps: the eps value in layer normalization components (default=1e-5).
+
     Examples::
         >>> encoder_layer = nn.TransformerEncoderLayer(d_model=512, nhead=8)
         >>> query = torch.rand(10, 32, 512)
@@ -24,7 +25,7 @@ class GeneralTransformerEncoderLayer(nn.Module):
         >>> out = encoder_layer(query, key, value)
     """
 
-    def __init__(self, d_model, nhead, dim_feedforward=2048, dropout=0.1, activation="relu", layer_norm_eps=1e-5):
+    def __init__(self, d_model, nhead, dim_feedforward=2048, dropout=0.1, activation="relu"):
         super(GeneralTransformerEncoderLayer, self).__init__()
         self.multihead_attn = nn.MultiheadAttention(d_model, nhead, dropout=dropout)
         # Implementation of Feedforward model
@@ -32,8 +33,8 @@ class GeneralTransformerEncoderLayer(nn.Module):
         self.dropout = nn.Dropout(dropout)
         self.linear2 = nn.Linear(dim_feedforward, d_model)
 
-        self.norm1 = nn.LayerNorm(d_model, eps=layer_norm_eps)
-        self.norm2 = nn.LayerNorm(d_model, eps=layer_norm_eps)
+        self.norm1 = nn.LayerNorm(d_model)
+        self.norm2 = nn.LayerNorm(d_model)
         self.dropout1 = nn.Dropout(dropout)
         self.dropout2 = nn.Dropout(dropout)
 
@@ -62,9 +63,9 @@ class GeneralTransformerEncoderLayer(nn.Module):
         Shape:
             see the docs in Transformer class.
         """
-        if not key:
+        if key is None:
             key = query
-        if not value:
+        if value is None:
             value = query
         query2 = self.multihead_attn(query, key, value, attn_mask=src_mask, key_padding_mask=src_key_padding_mask)[0]
         query = query + self.dropout1(query2)
@@ -80,3 +81,46 @@ def _get_activation_fn(activation):
         return F.relu
     elif activation == "gelu":
         return F.gelu
+
+
+class CoAttentionModule(nn.Module):
+    def __init__(
+        self, coattention_transformer_config: dict, output_transformer_config: dict, num_of_combined_coattention_and_output_transformers: int = 1
+    ):
+        super(CoAttentionModule, self).__init__()
+        self.coattention_transformer_config = coattention_transformer_config
+        self.output_transformer_config = output_transformer_config
+        self.num_of_combined_coattention_and_output_transformers = num_of_combined_coattention_and_output_transformers
+
+        self.vision_coattention_models = _get_clones(
+            GeneralTransformerEncoderLayer(**self.coattention_transformer_config), self.num_of_combined_coattention_and_output_transformers
+        )
+
+        self.vision_output_transformer_models = _get_clones(
+            nn.TransformerEncoderLayer(**self.output_transformer_config), self.num_of_combined_coattention_and_output_transformers
+        )
+
+        self.text_coattention_models = _get_clones(
+            GeneralTransformerEncoderLayer(**self.coattention_transformer_config), self.num_of_combined_coattention_and_output_transformers
+        )
+
+        self.text_output_transformer_models = _get_clones(
+            nn.TransformerEncoderLayer(**self.output_transformer_config), self.num_of_combined_coattention_and_output_transformers
+        )
+
+    def forward(self, image_embedding: torch.Tensor, text_embedding: torch.Tensor):
+
+        image_output = image_embedding
+        text_output = text_embedding
+        for i in range(self.num_of_combined_coattention_and_output_transformers):
+            intermediate_image_embedding = self.vision_coattention_models[i](query=image_output, key=text_output, value=text_output)
+            intermediate_text_embedding = self.text_coattention_models[i](query=text_output, key=image_output, value=image_output)
+
+            image_output = self.vision_output_transformer_models[i](intermediate_image_embedding)
+            text_output = self.text_output_transformer_models[i](intermediate_text_embedding)
+
+        return image_output, text_output
+
+
+def _get_clones(module, N):
+    return nn.ModuleList([copy.deepcopy(module) for i in range(N)])
